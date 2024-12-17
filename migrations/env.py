9d021762dfb137fdac6feec5916015a595173728
Sergey Golobokov <sergeygolobokov@gmail.com>
2024-12-17 17:19:28 +0300

    add User migration

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app.core.config import settings
-from app.core.database import *
+from app.core import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +24,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-config.set_main_option('sqlalchemy.url', settings.postgres.url)
+config.set_main_option('sqlalchemy.url', settings.postgres.async_url)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
